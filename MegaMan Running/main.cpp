deae5a5bd452c3a6718a9799cf8a13565be0534a
Riel42 <gabryel.reis@alunos.ifsuldeminas.edu.br>
2022-08-08 19:15:23 -0300

    New files

--- MegaMan Running/main.cpp
+++ MegaMan Running/main.cpp
@@ -1,4 +1,5 @@
 #include "SFML/Graphics.hpp"
+#include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Texture.hpp>
@@ -14,9 +15,13 @@ int main() {
   sf::Texture texture;
   texture.loadFromFile("resources/mega.png");
 
-  sf::Sprite sprite(texture, sf::IntRect(0,0,85,70));
+  sf::IntRect rect(0,0,85,70); 
 
+  sf::Sprite sprite(texture, sf::IntRect(rect));
+  //Ao invés de colocar os valores dentro desse InteRect, eu coloco seu objeto correspondente,
+  //assim, eu consigo variá-los conforme o player se move
 
+  sf::Clock clock; //objeto para medir o tempo do jogo
 
   while (window.isOpen()) {
 
@@ -24,6 +29,22 @@ int main() {
       if (event.type == sf::Event::EventType::Closed) {
         window.close();
       }
+    }
+
+    if (clock.getElapsedTime().asSeconds() > 0.2f){ //Quanto maior o valor, mais lenta a animação
+    
+    //Se tempo decorrido em segundos for maior do que 0.2 segundos
+        if (rect.left == 255){
+        //Se o RectLeft for igual a 255 (85*3, que corresponde ao RectLeft do 4º sprite):
+            rect.left = 0; 
+            //RectLeft recebe 0 (o RectLeft volta para o início, ou seja, volta para o 1º sprite)
+        }
+        else rect.left += 85;
+
+        sprite.setTextureRect(rect); //Atualiza o sprite a cada frame
+        
+        clock.restart(); //Passou de 0.2 segundos, o clock volta para zero, para não deixar o nosso
+                         //personagem correndo infinitamente
     }
 
     window.clear();
